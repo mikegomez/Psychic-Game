b80f3799b4da857d6c74c83f0aa4c04fe21df8db
Mike Gomez <mikegomez21@hotmail.com>
2019-11-16 22:54:02 -0600

    restructure files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,3 @@
-<script>
         //Letter available
  var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
  
@@ -68,4 +67,3 @@
          }
  };
             
-       </script>
